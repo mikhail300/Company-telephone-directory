04741822f131e75da70d12fb3225da4917d983c7
Mikhail <xend@tut.by>
2022-07-15 04:21:47 +0300

    DivisionsController ready

--- Company telephone directory/Controllers/DivisionsController.cs
+++ Company telephone directory/Controllers/DivisionsController.cs
@@ -11,7 +11,41 @@ namespace Company_telephone_directory.Controllers
     [ApiController]
     public class DivisionsController : ControllerBase
     {
-     
+        //api/divisions/byId?id=1
+        [HttpGet("byId")]
+        public List<Division> GetById(int id)
+        {
+            string sql = "SELECT id, title, parent_id FROM divisions WHERE id = " + id;
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            List<Division> divisions = new List<Division>();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (DbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            divisions.Add(new Division(Convert.ToInt32(reader.GetValue(0)), reader.GetString(1), Convert.ToInt32(reader.GetValue(2))));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return divisions;
+        }
+
         //api/divisions/All
         [HttpGet("All")]
         public List<Division> GetAll()
@@ -47,6 +81,132 @@ namespace Company_telephone_directory.Controllers
             return divisions;
         }
 
+        //api/divisions/byParent_id?parent_id=1
+        [HttpGet("byParent_id")]
+        public List<Division> GetByParent_id(int parent_id)
+        {
+            string sql = "SELECT id, title, parent_id FROM divisions WHERE parent_id = " + parent_id;
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            List<Division> divisions = new List<Division>();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (DbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            divisions.Add(new Division(Convert.ToInt32(reader.GetValue(0)), reader.GetString(1), Convert.ToInt32(reader.GetValue(2))));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return divisions;
+        }
+
+
+        // POST api/divisions/add
+        [HttpPost("add")]
+        public NonQueryResponse PostData(string title, int parent_id)
+        {
+            string sql = "INSERT INTO divisions (title, parent_id) VALUES ('" + title + "', '" + parent_id + "')";
+            NonQueryResponse nonQueryResponse = new NonQueryResponse();
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                nonQueryResponse.numberOfRecords = cmd.ExecuteNonQuery();
+                nonQueryResponse.lastInsertedId = cmd.LastInsertedId;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return nonQueryResponse;
+        }
+
+        //api/divisions/editById
+        [HttpPut("editById")]
+        public NonQueryResponse PutData(int id, string title, int parent_id)
+        {
+            NonQueryResponse nonQueryResponse = new NonQueryResponse();
+            if (id == 1)
+            {
+                nonQueryResponse.numberOfRecords = 0;
+                return nonQueryResponse;
+            }
+            string sql = "UPDATE divisions SET title = '" + title + "', parent_id = '" + parent_id + "' WHERE (id = '" + id + "');";
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                nonQueryResponse.numberOfRecords = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return nonQueryResponse;
+        }
+
+
+        //api/division/deleteById
+        [HttpDelete("deleteById")]
+        public NonQueryResponse DeleteData(int id)
+        {
+            NonQueryResponse nonQueryResponse = new NonQueryResponse();
+
+            if (id == 1)
+            {
+                nonQueryResponse.numberOfRecords = 0;
+                return nonQueryResponse;
+            }
+            string sql = "DELETE FROM divisions WHERE (id = '" + id + "');";
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                nonQueryResponse.numberOfRecords = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return nonQueryResponse;
+        }
     }
 }
 
---
+++ Company telephone directory/Models/NonQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Company_telephone_directory.Models
+{
+    public class NonQueryResponse
+    {
+        public int numberOfRecords { set; get; }
+        public long lastInsertedId { set; get; }
+    }
+}
